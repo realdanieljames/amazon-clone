0d1eca117bead46a8e6e69f19243f49eb57dce20
DANIEL JAMES <daniel.james@codeimmersives.com>
2021-06-04 20:40:03 -0700

    set up next config js to whitelist image locations

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import Image from "next/image"
+
+function Header() {
+    return (
+        <header>
+            {/* Top Nav */}
+            <div>
+                <div>
+                    <Image
+                        src='https://links.papareact.com/f90'
+                        width={150}
+                        height={40}
+                    />
+                </div>
+            </div>
+
+            {/* Bottom Nav */}
+            <div></div>
+        </header>
+    )
+}
+
+export default Header
