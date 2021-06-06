cd7181279bc5b3031b72a2c3bc85e6122ad488fe
DANIEL JAMES <daniel.james@codeimmersives.com>
2021-06-06 11:50:55 -0700

    added product feed with css

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -1,12 +1,20 @@
-
+import Product from "./Product"
 
 
 function ProductFeed({products}) {
     return (
-        <div>
-            <h1> hdifdfp</h1>
-            {products.map((product)=> (
-                <p>{product.title}</p>
+        <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {/* <h1>Products here ...</h1> */}
+            {products.map(({id, title, price, description, category, image})=> (
+                <Product
+                    key={id}
+                    id={id}
+                    title={title}
+                    price={price}
+                    description={description}
+                    category={category}
+                    image={image}
+                />
             ))}
         </div>
     )
