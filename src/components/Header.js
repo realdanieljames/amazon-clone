eb400a9998e835946419b93ed2bcaeaabc3ff294
DANIEL JAMES <daniel.james@codeimmersives.com>
2021-06-06 16:08:00 -0700

    added checkout page component and added page routing for home on amazon logo and the basket icon routes  to  checkout page

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,13 @@ MenuIcon,
 SearchIcon,
 ShoppingCartIcon,
 } from "@heroicons/react/outline";
-import { signIn, signout, signOut, useSession } from "next-auth/client";
+import { signIn, signOut, useSession } from "next-auth/client";
+import {useRouter} from "next/router";
+
 
 function Header() {
 const [session] = useSession();
+const router = useRouter();
 
 return (
     <header>
@@ -16,6 +19,7 @@ return (
     <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
         <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
         <Image
+            onClick={()=> router.push('/')}
             src="https://links.papareact.com/f90"
             width={150}
             height={40}
@@ -47,7 +51,7 @@ return (
             <p className="font-extrabold md:text-sm">& Orders</p>
         </div>
 
-        <div className="relative link flex items-center">
+        <div onClick={()=> router.push('/checkout')} className="relative link flex items-center">
             <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black">
             0
             </span>
---
+++ src/pages/checkout.js
@@ -0,0 +1,19 @@
+import Header from "../components/Header"
+
+
+
+function checkout() {
+    return (
+        <div className="bg-gray-100">
+            <Header/>
+            <h1>Checkout pag</h1>
+        </div>
+    )
+}
+
+export default checkout
+
+
+
+
+
