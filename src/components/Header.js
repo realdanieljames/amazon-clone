ce8efdbc00d6aa66db7017a43f2b91362bb393b3
DANIEL JAMES <daniel.james@codeimmersives.com>
2021-06-06 17:49:43 -0700

    users can now add items to basket and have them show up in the checkout page. From the checkout page they can now add more of the same item or remove item from basket

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,11 +6,14 @@ ShoppingCartIcon,
 } from "@heroicons/react/outline";
 import { signIn, signOut, useSession } from "next-auth/client";
 import {useRouter} from "next/router";
+import {useSelector } from "react-redux"
+import { selectItems } from "../slices/basketSlice";
 
 
 function Header() {
 const [session] = useSession();
 const router = useRouter();
+const items = useSelector(selectItems);
 
 return (
     <header>
@@ -53,7 +56,7 @@ return (
 
         <div onClick={()=> router.push('/checkout')} className="relative link flex items-center">
             <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black">
-            0
+            {items.length}
             </span>
             <ShoppingCartIcon className="h-10" />
             <p className="hidden md:inline font-extrabold md:text-sm mt-2">
